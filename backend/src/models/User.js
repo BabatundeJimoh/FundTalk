7182725441878180700e1e1e24df5668bcfe8787
Kelvin Akaba <akabakelvin@gmail.com>
2023-09-10 16:09:08 +0000

    client & server connection

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -2,11 +2,6 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const UserSchema = new Schema({
-    username: {
-        type: String, 
-        required: true,
-        unique: true
-    },
     name: {
         type: String,
         required: true
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,10 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-
 import Routing from "./Routing";
+import axios from 'axios'
+
+axios.defaults.baseURL = 'http://localhost:3001'
+axios.defaults.withCredentials = true
 
 function App() {
   return (
