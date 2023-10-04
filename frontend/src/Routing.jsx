97aa4ac5d51a544966aaa1c168479ff1377a2906
babatunde <benjee135333@gmail.com>
2023-10-04 01:35:43 +0000

    FirstPage

--- frontend/src/Routing.jsx
+++ frontend/src/Routing.jsx
@@ -3,12 +3,14 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./component/Home";
 import SignUp from "./component/SignUp";
 import Dashboard from "./component/Dashboard";
+import FirstPage from "./component/FirstPage";
 
 function Routing() {
   return (
     <div>
       <Routes>
-        <Route exact path="/" element={<Home />} />
+        <Route path="/" element={<FirstPage />} />
+        <Route exact path="/login" element={<Home />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
